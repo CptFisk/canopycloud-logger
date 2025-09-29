3726e297397478e695e81cc9d564efcb67fa79fb
marcus <marcus@kammarfelt.eu>
2025-09-29 14:17:19 +0200

    Added virtual init

--- interface/base/include/base.hpp
+++ interface/base/include/base.hpp
@@ -1,18 +1,21 @@
 #pragma once
-#include <types.hpp>
-
 #include "types.hpp"
 
+#include <types.hpp>
+
 namespace Interface {
-    /**
-     * @brief Baseclass for all communication interfaces
-     */
-    class Base {
-    public:
-        auto getType() const -> Types;
-        virtual auto connect() -> bool = 0;
-        virtual auto read() -> bool = 0;
-    private:
-        Types type;
-    };
+/**
+ * @brief Baseclass for all communication interfaces
+ */
+class Base {
+  public:
+    auto getType() const -> Types;
+
+    virtual auto init() -> bool    = 0;
+    virtual auto connect() -> bool = 0;
+    virtual auto read() -> bool    = 0;
+
+  private:
+    Types type;
+};
 }

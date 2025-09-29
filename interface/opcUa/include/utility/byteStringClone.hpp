20478d9e96fcbe794beb2dee6ca3c00186750515
marcus <marcus@kammarfelt.eu>
2025-09-29 05:21:00 +0200

    FUnction to clone UA_Byte

---
+++ interface/opcUa/include/utility/byteStringClone.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <open62541/types.h>
+
+namespace Interface {
+
+class ByteStringClone {
+  public:
+    explicit ByteStringClone(const UA_ByteString& source);
+    ~ByteStringClone();
+
+    // Overloaders
+    ByteStringClone(const ByteStringClone&)            = delete;
+    ByteStringClone& operator=(const ByteStringClone&) = delete;
+    ByteStringClone(ByteStringClone&& other) noexcept;
+    ByteStringClone& operator=(ByteStringClone&& other) noexcept;
+
+    auto getValue() const -> UA_ByteString;
+
+  private:
+    UA_ByteString value;
+};
+
+}
---
+++ interface/opcUa/src/utility/byteStringClone.cpp
@@ -0,0 +1,42 @@
+#include <cstring>
+#include <utility/byteStringClone.hpp>
+
+namespace Interface {
+
+ByteStringClone::ByteStringClone(const UA_ByteString& source)
+  : value{} {
+    value.length = source.length;
+    value.data   = static_cast<UA_Byte*>(malloc(value.length * sizeof(UA_ByteString)));
+    if (value.data && source.data) {
+        std::memcpy(value.data, source.data, value.length);
+    }
+}
+
+ByteStringClone::~ByteStringClone() {
+    UA_ByteString_clear(&value);
+}
+
+auto
+ByteStringClone::getValue() const -> UA_ByteString {
+    return value;
+}
+
+ByteStringClone::ByteStringClone(ByteStringClone&& other) noexcept
+  : value{} {
+    value              = other.value;
+    other.value.length = 0;
+    other.value.data   = nullptr;
+}
+
+ByteStringClone&
+ByteStringClone::operator=(ByteStringClone&& other) noexcept {
+    if (this != &other) {
+        UA_ByteString_clear(&value);
+        value              = other.value;
+        other.value.length = 0;
+        other.value.data   = nullptr;
+    }
+    return *this;
+}
+
+}

dd0787a4793a38e0c1c2662ebc7392232f8bbfaa
marcus <marcus@kammarfelt.eu>
2025-09-29 15:28:23 +0200

    Finally working

--- interface/opcUa/include/opc.hpp
+++ interface/opcUa/include/opc.hpp
@@ -1,32 +1,41 @@
 #pragma once
+#include <base.hpp>
 #include <config.hpp>
 #include <open62541/client_config_default.h>
 #include <open62541/client_highlevel.h>
-#include <open62541/plugin/securitypolicy_default.h>
 #include <open62541/plugin/log_stdout.h>
+#include <open62541/plugin/securitypolicy_default.h>
 #include <string>
-#include <base.hpp>
+#include <utility/byteStringClone.hpp>
+#include <utility/byteStringLoad.hpp>
 #include <vector>
 
 namespace Interface {
 
 class OpcUa : public Base {
-public:
+  public:
     OpcUa(const OpcConfig& config);
+    ~OpcUa() = default;
 
+    auto init() -> bool override;
     auto connect() -> bool override;
-
     auto read() -> bool override;
 
-private:
-protected:
+  private:
+    auto fetchAndSelectCertificate() -> bool;
+
+  protected:
+    const OpcConfig config;
+
     UA_Client*       client;       ///< Client
     UA_ClientConfig* clientConfig; ///< Configuration
     UA_StatusCode    status;       ///< Status code
 
+    ByteStringClone serverCert; ///< OPC-UA server certificate
+    ByteStringLoad  clientCert; ///< Interface certificate
+    ByteStringLoad  keyCert;    ///< Interface key
+
     const std::string           endpoint;
     std::vector<UA_ReadValueId> nodes; ///< Nodes to read
-
-    auto security(const Security& security) -> void;
 };
 }
--- interface/opcUa/src/opc.cpp
+++ interface/opcUa/src/opc.cpp
@@ -1,55 +1,77 @@
+#include <cert.hpp>
 #include <iostream>
 #include <opc.hpp>
-#include <cert.hpp>
-#include <utility.hpp>
-
+#include <utility/convert.hpp>
 
 namespace Interface {
 
-OpcUa::OpcUa(const OpcConfig& config) : endpoint(config.uri), status{}, client(nullptr), clientConfig(nullptr) {
-    client = UA_Client_new();
-
-    if (!client)
-        throw std::runtime_error("Failed to create UA_Client");
-
-    //Setup security
-    security(config.security);
-
-    X509Certificate cert;
-    cert.generate(2048, 365 * 2, "My OPC");
+OpcUa::OpcUa(const OpcConfig& config)
+  : endpoint(config.uri)
+  , status{}
+  , client(nullptr)
+  , clientConfig(nullptr)
+  , config(config) {
+    // Initialize
+    client       = UA_Client_new();
+    clientConfig = UA_Client_getConfig(client);
+    UA_ClientConfig_setDefault(clientConfig);
 }
 
 auto
-OpcUa::security(const Security& security) -> void {
-    clientConfig = UA_Client_getConfig(client);
-    UA_ClientConfig_setDefault(clientConfig);
+OpcUa::init() -> bool {
+    if (!fetchAndSelectCertificate())
+        return false;
 
-    const auto config               = getSecurityConfig(security);
-    clientConfig->securityMode      = config.policyType;
-    clientConfig->securityPolicyUri = UA_STRING_ALLOC(config.policyUri.c_str());
+    // Create certificates
+    X509Certificate cert;
+    cert.generate(2048, 365, "Canopy", "client.der", "key.der");
+    // Load files
+    clientCert.set("client.der");
+    keyCert.set("key.der");
 
-    if (!security == Security::None) {
-        X509Certificate certificate;
-        certificate.generate(2048, 365 * 2, "Logger");
-        //We need to create and load certificates
-        UA_ByteString cert = ByteStringWrapper("cert.der").value;
-        UA_ByteString key  = ByteStringWrapper("key.der").value;
+    // Configure encryption
+    const UA_ByteString trustList[] = { serverCert.getValue() };
 
-        UA_ClientConfig_setDefaultEncryption(clientConfig,
-                                     "cert.der",
-                                     "key.der",
-                                     "trusted/",  // katalog med servercertifikat
-                                     nullptr);    // ingen CRL
-
-
+    if (UA_ClientConfig_setDefaultEncryption(clientConfig, clientCert.getValue(), keyCert.getValue(), trustList, 1, nullptr, 0) !=
+        UA_STATUSCODE_GOOD) {
+        std::cerr << "Kunde inte konfigurera kryptering: " << UA_StatusCode_name(status) << std::endl;
+        UA_Client_delete(client);
+        return false;
     }
-
+    return true;
 }
 
+auto
+OpcUa::fetchAndSelectCertificate() -> bool {
+    size_t                  endpointCount = {};
+    UA_EndpointDescription* endpoints;
+
+    // Fetch server certificate
+    if (UA_Client_getEndpoints(client, endpoint.c_str(), &endpointCount, &endpoints) != UA_STATUSCODE_GOOD || endpointCount == 0) {
+        return false;
+    }
+    UA_EndpointDescription* selected = nullptr;
+    auto                    security = getSecurityConfig(config.security);
+
+    bool matched = false;
+    for (auto i = 0; i < endpointCount; ++i) {
+        if (endpoints[i].securityMode == security.policyType && uaToStdString(endpoints[i].securityPolicyUri) == security.policyUri) {
+            selected = &endpoints[i];
+            matched  = true;
+            break;
+        }
+    }
+
+    if (!matched)
+        return false;
+
+    serverCert.set(selected->serverCertificate);
+    return true;
+}
 
 auto
 OpcUa::connect() -> bool {
-    status = UA_Client_connect(client, endpoint.c_str());
+    status = UA_Client_connect(client, config.uri.c_str());
     if (status != UA_STATUSCODE_GOOD)
         throw std::runtime_error("Failed to connect to UA_Client");
     return true;

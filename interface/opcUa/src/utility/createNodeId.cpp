7b894c76153229e29a8fe02f2c44363257042dc5
marcus <marcus@kammarfelt.eu>
2025-10-03 10:46:02 +0200

    Missed to catch regex

--- interface/opcUa/src/utility/createNodeId.cpp
+++ interface/opcUa/src/utility/createNodeId.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <regex>
 #include <utility/createNodeId.hpp>
 
@@ -7,10 +8,10 @@ auto
 createNodeId(const std::string& address) -> std::optional<UA_NodeId> {
     std::smatch match; // Match
 
-    const std::regex numeric(R"(ns=\d+;i=\d+)");
-    const std::regex string(R"(ns=\d+;s=[\w\.\-]+)");
-    const std::regex guid(R"(ns=\d+;g=[0-9a-fA-F\-]{36})");
-    const std::regex opaque(R"(ns=\d+;b=[A-Za-z0-9+/=]+)");
+    const std::regex numeric(R"(ns=(\d+);i=(\d+))");
+    const std::regex string(R"(ns=(\d+);s=([\w\.\-]+))");
+    const std::regex guid(R"(ns=(\d+);g=([0-9a-fA-F\-]{36}))");
+    const std::regex opaque(R"(ns=(\d+);b=([A-Za-z0-9+/=]+))");
 
     if (std::regex_match(address, match, numeric))
         return UA_NODEID_NUMERIC(std::stoi(match[1]), std::stoi(match[2]));

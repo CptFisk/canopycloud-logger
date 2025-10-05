3c4bb4baebed515225f1c24234a6a591d4689999
marcus <marcus@kammarfelt.eu>
2025-10-05 09:38:25 +0200

    Added no-discard and const

--- interface/base/include/base.hpp
+++ interface/base/include/base.hpp
@@ -11,8 +11,8 @@ namespace Interface {
  * @brief Container to hold tag-values.
  */
 struct BaseTag {
-    std::string                                                                                           name;
-    std::string                                                                                           address;
+    const std::string                                                                                           name;
+    const std::string                                                                                           address;
     std::variant<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, float, double, bool, std::string> value;
     int                                                                                                   polling;
     BaseTag(const std::string& name, const std::string& address, const int& polling)
@@ -26,11 +26,11 @@ struct BaseTag {
  */
 class Base {
   public:
-    virtual auto init() -> bool    = 0;
-    virtual auto connect() -> bool = 0;
-    virtual auto read() -> bool    = 0;
+    [[nodiscard]] virtual auto init() -> bool    = 0;
+    [[nodiscard]] virtual auto connect() -> bool = 0;
+    [[nodiscard]] virtual auto read() -> bool    = 0;
 
-    auto getTags() -> std::vector<BaseTag>& { return baseTags; }
+    [[nodiscard]] auto getTags() -> std::vector<BaseTag>& { return baseTags; }
 
   protected:
     std::vector<BaseTag> baseTags;
